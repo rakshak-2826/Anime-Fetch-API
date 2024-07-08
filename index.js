2435386081189b30223751b1abd5dd3e07492743
Rakshak Datta <167508914+rakshak-2826@users.noreply.github.com>
2024-07-08 15:35:29 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,21 +2,22 @@ const express = require('express');
 const request = require('request');
 const app = express();
 
+// Middleware to set CORS headers
 app.use((req, res, next) => {
     res.header('Access-Control-Allow-Origin', '*');
     res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
     next();
 });
 
+// Endpoint to proxy requests
 app.get('/proxy', (req, res) => {
     const url = req.query.url;
     if (!url) {
         return res.status(400).send('URL is required');
     }
+
+    // Make a request to the provided URL and pipe the response back to the client
     request({ url, headers: { 'Origin': req.get('origin') } }).pipe(res);
 });
 
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-    console.log(`CORS proxy running on port ${PORT}`);
-});
+module.exports = app;
